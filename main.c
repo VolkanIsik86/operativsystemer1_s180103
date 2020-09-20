28429128eabaed895574e65b8fe148c6d3b8b3bf
volkan <volkanisik@live.dk>
2020-09-20 03:20:04 +0200

    almost done pipe remains

--- main.c
+++ main.c
@@ -1,83 +1,66 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <wait.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 #include <string.h>
 
 #define TRUE 1
 
-
-char * inputarr[10];
-int * arguments;
-int * status;
-
 void type_prompt();
-void read_command(char *command , char *parameters);
+void read_command(char * shellInput[]);
 
 int main() {
 
     while (TRUE) {                                /* repeat forever */
+        int pid;
         type_prompt();                          /* display promt on the screen */
 
-        char command[20];
-        char parameters[80];
-        read_command(&command, &parameters);      /* read input from terminal */
+        char *shellInput[20];
 
-        printf("%s\n", *command);
-        printf("%s\n", *parameters);
+        read_command(shellInput);      /* read input from terminal */
 
-        if (fork() != 0) {                         /* fork off child process */
+        int exit = strcmp(shellInput[0],"exit");
+        if(exit == 0){
+            break;
+        }
+
+        pid = fork();
+        if (pid > 0) {                         /* fork off child process */
             /* parent code*/
-            waitpid(-1, &status, 0);             /* wait for child to exit */
+           pid = wait(NULL);             /* wait for child to exit */
+           printf("Type new command or type exit to leave\n");
         } else {
             /*child code */
-            execve(command, parameters, 0);      /* execute command */
-
-
+            execvp(shellInput[0], shellInput);      /* execute command */
         }
-
-        return 0;
     }
+    return 0;
 }
 
-
-void type_prompt(){
+void type_prompt() {
     printf("$ ");
-    char* input= malloc(sizeof(char)*100);
-    gets(input);
-
-   arguments = malloc(sizeof(int)*2);
-   arguments[0] = 0;
-
-    char *inputtokens = strtok(input," ");
-
-    while(inputtokens != NULL) {
-        inputarr[arguments[0]] = inputtokens;
-        arguments[0]++;
-        inputtokens = strtok(NULL," ");
-    }
-
-
 }
+    void read_command(char *shellInput[]) {
 
-void read_command(char *command , char *parameters){
-    command = inputarr[0];
-    printf("%s\n",command);
+        int maxlength = 100;
+        char *input = malloc(sizeof(char) * maxlength);
+        int wordCount = 0;
 
-    printf("%d\n",arguments[0]);
+        scanf("%[^\n]%*c",input);
 
-    char * param = calloc(81, sizeof(char));
-    for (int i = 1; i < arguments[0] ; ++i) {
-        strcat(param,inputarr[i]);
-        if (i!=(arguments[0]-1)) {
-            strcat(param, " ");
+       // fgets(input, 100, stdin);
+        fflush(stdin);
+        fflush(stdout);
+
+        char *args = strtok(input," ");
+
+        while (args != NULL ){
+            shellInput[wordCount] = strdup(args);
+            args = strtok(NULL," ");
+            wordCount++;
         }
+        shellInput[wordCount] = NULL;
+
+    free(input);
     }
-
-    parameters = param;
-
-
-
-
-}
